9d698d1cf2e339427bc2fb4c8a2b2e1884f3ec9d
repoimran <imsdhk@gmail.com>
2023-06-30 17:35:16 -0400

    first commit

---
+++ target/test-classes/karate-config.js
@@ -0,0 +1,21 @@
+function fn() { 
+var env = karate.env; // get system property 'karate.env' karate.log('karate.env system property was:', env); 
+if (!env) { 
+env = 'dev'; 
+} 
+
+var config = { 
+env: env, 
+myVarName: 'hello karate', 
+baseUrl: 'https://gorest.co.in',
+tokenID: '65a8e6f29be519198d66d75e845550d342c2c2442286b00b7ea06ac181a497a6'
+}
+
+if (env == 'dev') { 
+// customize 
+// e.g. config.foo = 'bar'; 
+} else if (env == 'e2e') { 
+// customize 
+} 
+return config; 
+}
